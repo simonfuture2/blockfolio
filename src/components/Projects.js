63982b535c8ac58c1deb4e85ecf9faefac040eca
Simon <78990353+simonfuture2@users.noreply.github.com>
2022-09-03 10:58:14 -0400

    Add files via upload

---
+++ src/components/Projects.js
@@ -0,0 +1,53 @@
+// Import Assets
+import uniswap from '../assets/uniswap.png';
+import compound from '../assets/compound.png';
+import aave from '../assets/aave.png';
+
+const Projects = () => {
+    return (
+        <section className="projects">
+            <h2>My Projects</h2>
+
+            <div className="projects__cards">
+
+                <div className="projects__card">
+                    <h3>Uniswap</h3>
+                    <img src={uniswap} alt="Uniswap Swap Page" />
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Facilis consectetur facere neque assumenda quasi aliquid,
+                        perferendis ratione rerum dolorem.
+                    </p>
+
+                    <a href="https://uniswap.org/" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/Uniswap/v2-core" target="_blank" className="button">Code</a>
+                </div>
+
+                <div className="projects__card">
+                    <h3>Compound</h3>
+                    <img src={compound} alt="Compound Landing Page" />
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Facilis consectetur facere neque assumenda quasi aliquid,
+                        perferendis ratione rerum dolorem.
+                    </p>
+
+                    <a href="https://compound.finance/" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/compound-finance/compound-protocol" target="_blank" className="button">Code</a>
+                </div>
+
+                <div className="projects__card">
+                    <h3>Aave</h3>
+                    <img src={aave} alt="Aave Landing Page" />
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Facilis consectetur facere neque assumenda quasi aliquid,
+                        perferendis ratione rerum dolorem.
+                    </p>
+
+                    <a href="https://aave.com/" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/aave/aave-v3-core" target="_blank" className="button">Code</a>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Projects;
