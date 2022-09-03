519c77c39fc539c231d64a8ec190501351c2bc1f
Simon <78990353+simonfuture2@users.noreply.github.com>
2022-09-03 16:24:06 -0400

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="footer">
-            <p><small>John Doe &copy; 2022</small></p>
-        </footer>
-    );
-}
-
-export default Footer;
