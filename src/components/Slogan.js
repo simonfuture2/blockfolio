a658fb9f707e3a291c20e6a3914ada00801da841
Simon <78990353+simonfuture2@users.noreply.github.com>
2022-09-22 11:04:26 -0400

    Update Slogan.js

--- src/components/Slogan.js
+++ src/components/Slogan.js
@@ -5,8 +5,8 @@ const Slogan = () => {
     return (
         <section className='Slogan'>
             <img src={logo2} alt="Simon Batrony" 
-                width="750" 
-                height="250" />
+                width="75%" 
+                height="25%" />
 
             <div className='Slogan__content'>
                 <h3>Enhance Your FutureSELF</h3>
