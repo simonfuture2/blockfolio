2ab884b552a542fb8f12a1a7ae9361abf2ceae07
Simon <78990353+simonfuture2@users.noreply.github.com>
2022-10-24 08:41:59 -0400

    Update Social.js

--- src/components/Social.js
+++ src/components/Social.js
@@ -8,8 +8,10 @@ const Social = () => {
             <a href="https://github.com/simonfuture2" target="_blank" className="button">GitHub</a>
             <a href="http://portfolio.simonfuture.hns.to/" target="_blank" className="button">dLink-Simon</a>
             <a href="http://cryptechology.hns.to/" target="_blank" className="button">Cryptechology</a> 
+            <iframe allowfullscreen="allowfullscreen" src="https://designrr.page?id=236006&token=348591333&type=FP&h=3856" height="600" width="800" frameborder="0"></iframe>
+    );
         </div>
-        <iframe allowfullscreen="allowfullscreen" src="https://designrr.page?id=236006&token=348591333&type=FP&h=3856" height="600" width="800" frameborder="0"></iframe>
+       
     );
 }
 
